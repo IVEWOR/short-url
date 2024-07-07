67d6c0c17a28740a805b091d2a251d86b1b3963a
IVEWOR <howdy@deepslog.com>
2024-07-08 01:36:57 +0530

    displaying table of containing shortids and visitis

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
 const express = require("express");
+const path = require("path");
 
 const connectDB = require("./connect.db");
 const urlRoute = require("./routes/url");
-const staticRouter = require("./routes/staticRouter")
+const staticRouter = require("./routes/staticRouter");
 const URL = require("./models/url");
 
 const app = express();
 
 const PORT = 8000;
 
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
+app.set("views", path.resolve("./views"));
 
 // Connecting to DB
 connectDB("mongodb://127.0.0.1:27017/short-url").then(
@@ -17,12 +19,12 @@ connectDB("mongodb://127.0.0.1:27017/short-url").then(
 );
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }))
+app.use(express.urlencoded({ extended: false }));
 
 app.use("/", staticRouter);
 app.use("/url", urlRoute);
 
-app.get("/:shortID", async (req, res) => {
+app.get("/url/:shortID", async (req, res) => {
   const shortID = req.params.shortID;
   const entry = await URL.findOneAndUpdate(
     {
@@ -38,7 +40,7 @@ app.get("/:shortID", async (req, res) => {
   );
 
   if (!entry) {
-    return res.status(404).json({ "error": "short url not found" });
+    return res.status(404).json({ error: "short url not found" });
   }
   res.redirect(entry.redirectURL);
 });
--- routes/staticRouter.js
+++ routes/staticRouter.js
@@ -1,9 +1,13 @@
 const express = require("express");
+const URL = require("../models/url");
 
 const router = express.Router();
 
-router.get("/", (req, res) => {
-    res.render("home");
+router.get("/", async (req, res) => {
+  const allURLs = await URL.find({});
+  res.render("home", {
+    urls: allURLs,
+  });
 });
 
 module.exports = router;
